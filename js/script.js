3e302962c9eecdd35c15260b51392c5c2c345cbc
Beau Gunderson <beau@beaugunderson.com>
2012-02-20 00:31:00 -0800

    Getting there. Add Bootstrap for looks, a throbber, beginning to work out some kind of graphing framework.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-function explore(collection) {
-   $.getJSON(collection.url, { 'limit': 1000 }, function(data) {
+var jsonThrobber;
+
+function addFields(collection, eachCallback, allCallback) {
+   return $.getJSON(collection.url, { 'limit': 1000 }, function(data) {
       var fields = [];
 
       _.chain(data).shuffle().first(100).each(function(entry) {
@@ -7,7 +9,7 @@ function explore(collection) {
       });
 
       _.each(fields, function(field) {
-         field.name = field.name.replace(/\.\d+(\.|$)/g, '.<em>index</em>$1');
+         field.name = field.name.replace(/\.\d+(\.|$)/g, '.<em>i</em>$1');
       });
 
       fields = _.uniq(fields, false, function(field) {
@@ -19,9 +21,12 @@ function explore(collection) {
       });
 
       collection.fields = fields;
+
+      eachCallback();
    });
 }
 
+// XXX refactor this
 function usableFields(obj, name) {
    function recurseUsableFields(obj, name, depth, results) {
       if (depth === undefined) {
@@ -62,68 +67,147 @@ function usableFields(obj, name) {
    return results;
 }
 
+function formatHistogramField(field) {
+   return sprintf('<li>' +
+      '%(name)s' +
+      '<a href="#">+value</a>' +
+      '</li>', field);
+}
+
+function updateCollection(collection) {
+   $('#numbers, #strings').html('');
+
+   _.chain(collection.fields).filter(function(field) { return field.type === "number"; }).each(function(field) {
+      $('#numbers').append(sprintf('<li>%(name)s</li>', field));
+   });
+
+   _.chain(collection.fields).filter(function(field) { return field.type === "string"; }).each(function(field) {
+      $('#strings').append(sprintf('<li>%(name)s</li>', field));
+   });
+}
+
+function startThrobber() {
+   if (jsonThrobber === undefined) {
+      jsonThrobber = new Throbber({
+         size: 20,
+         color: 'black'
+      });
+
+      // XXX
+      jsonThrobber.appendTo(document.getElementById('json-throbber'));
+   }
+
+   jsonThrobber.start();
+}
+
+function stopThrobber() {
+   if (jsonThrobber !== undefined) {
+      jsonThrobber.stop();
+   }
+}
+
+// The graph types and their respective options
+var graphTypes = {
+   table: {
+      fields: {
+         unlimited: ['number', 'string']
+      }
+   },
+   map: {
+      fields: {
+         latitude: {
+            types: ['number']
+         },
+         longitude: {
+            types: ['number']
+         },
+         label: {
+            types: ['number', 'string']
+         }
+      }
+   },
+   histogram: {
+      formatter: formatHistogramField,
+      fields: {
+         value: {
+            types: ['number']
+         }
+      }
+   },
+   scatter: {
+      fields: {
+         x: {
+            types: ['number']
+         },
+         y: {
+            types: ['number']
+         },
+         label: {
+            types: ['string']
+         }
+      }
+   }
+};
+
+var collections;
+
 $(function() {
    if (baseUrl === false) {
       window.alert("Couldn't find your locker, you might need to add a config.js (see <a href=\"https://me.singly.com/Me/devdocs/\">the docs</a>)");
    }
 
-   var placesUrl = baseUrl + '/Me/places/';
-   var contactsUrl = baseUrl + '/Me/contacts/';
-   var linksUrl = baseUrl + '/Me/links/';
-   var photosUrl = baseUrl + '/Me/photos/';
-
-   var collections = {
+   // The collection types and their respective URLs
+   collections = {
       places: {
          name: "Places",
          handle: "places",
-         url: placesUrl,
-         fields: []
+         url: baseUrl + '/Me/places/'
       },
       contacts: {
          name: "Contacts",
          handle: "contacts",
-         url: contactsUrl,
-         fields: []
+         url: baseUrl + '/Me/contacts'
       },
       links: {
          name: "Links",
          handle: "links",
-         url: linksUrl,
-         fields: []
+         url: baseUrl + '/Me/links/'
       },
       photos: {
          name: "Photos",
          handle: "photos",
-         url: photosUrl,
-         fields: []
+         url: baseUrl + '/Me/photos/'
       }
    };
 
-   _.each(collections, function(collection) {
-      $('#collections').append(sprintf('<option value="%(handle)s">%(name)s</option>', collection));
-
-      explore(collection);
-   });
-
+   // Update the UI when the user selects a collection type
    $('#collections').chosen().change(function() {
-      var option = $(this).val();
-
-      var collection = _.find(collections, function(c) {
-         return c.handle === option;
-      });
-
-      $('#numbers, #strings').html('');
-
-      _.chain(collection.fields).filter(function(field) { return field.type === "number"; }).each(function(field) {
-         $('#numbers').append(sprintf('<option value="%(name)s">%(name)s</option>', field));
-      });
-
-      _.chain(collection.fields).filter(function(field) { return field.type === "string"; }).each(function(field) {
-         $('#strings').append(sprintf('<option value="%(name)s">%(name)s</option>', field));
-      });
-
-      $("#fields").trigger("liszt:updated");
+      updateCollection(collections[$(this).val()]);
    });
 
-   $('#fields').chosen();
+   // Update the UI when the user selects a graph type
+   $('input[name=graph-type]').change(function() {
+      var checked = $('input[name=graph-type]:checked').eq(0).val();
+   });
+
+   var requests = [];
+
+   startThrobber();
+
+   // Get the data from Singly
+   _.each(collections, function(collection) {
+      // Populate the list of JSON fields with a sample of the actual data
+      requests.push(addFields(collection, function() {
+         // Add the collection type to the dropdown once we have the data
+         $('#collections').append(sprintf('<option value="%(handle)s">%(name)s</option>', collection));
+         $('#collections').trigger('liszt:updated');
+      }));
+   });
+
+   // We use apply here because requests is an array
+   // and $.when expects multiple arguments
+   $.when.apply($, requests).done(function() {
+      // Stop the throbber when all of the requests complete
+      stopThrobber();
+   });
 });
